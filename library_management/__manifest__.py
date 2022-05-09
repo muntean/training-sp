981b28c0ccd944deabc8d2a205b2abaf58727bed
Stefana Pop <stefana@information-systems.ro>
2022-05-09 13:21:08 +0300

    New module library_management

---
+++ library_management/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Management',
+
+    'summary': """App to manage Books and Customers""",
+
+    'description': """
+        Library Management App to organize books and rentals, and for customers to check out books.
+    """,
+
+    'author': 'Stefana Pop',
+
+    'website': 'https://www.information-systems.ro',
+
+    'category': 'Management',
+    'version': '1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ],
+}
