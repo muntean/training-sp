c71b4801766754f7c984ba25e02b523d485f6020
Stefana Pop <stefana@information-systems.ro>
2022-05-10 13:17:52 +0300

    Added fields to the Book model

--- library_management/models/book.py
+++ library_management/models/book.py
@@ -7,3 +7,15 @@ class Book(models.Model):
 
     _name = 'library.book'
     _description = 'Library Book'
+
+    name = fields.Char(string='Title', required=True)
+    author = fields.Char(string='Author')
+    editor = fields.Char(string='Editor')
+    publisher = fields.Char(string='Publisher')
+    edition_year = fields.Integer(string='Year of edition')
+    isbn = fields.Char(string='ISBN')
+    genre = fields.Selection(string='Genre',
+                             selection=[('mystery', 'Mystery'),
+                                        ('historical', 'Historical'),
+                                        ('romance', 'Romance'),
+                                        ('sci_fi', 'Science Fiction')])
